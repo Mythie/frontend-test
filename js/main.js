67bff95e094142eca2f46c289d8e84c4d3b2f5dc
Lucas Smith <mythiedev@gmail.com>
2018-10-29 09:34:11 +1100

    Cleaner solution to the slider issue

--- js/main.js
+++ js/main.js
@@ -3,54 +3,71 @@
 // that the layout will break.
 window.onload = function () {
   
-  const grid = document.getElementById('grid');
+  /* Slider Actions */
 
-  if(window.innerWidth > 767) {
-    // Using a lambda (arrow function) won't work here because it ruins the this scope.
-    imagesLoaded(grid, function () {
-      new Masonry(grid);
-    });
-  } else {
-    // Get the slider
-    const images = document.querySelectorAll('.image-item');
-    const bars = document.getElementById('slider-bars');
+  // Get the images we want to add to the slider.
+  const images = document.querySelectorAll('.image-item');
+  // Get the slider-bars
+  const bars = document.getElementById('slider-bars');
 
-    for (let i = 0; i < images.length; i++) {
-      document.getElementById('slider').append(images[i]);
+  // For every image
+  for (let i = 0; i < images.length; i++) {
+    // Clone it
+    const image = images[i].cloneNode();
+    // Remove the image-item class as it will display:none the image.
+    // Potentially remove all classes if we wanted this to be extendable.
+    image.classList.remove('image-item');
+    // Add the slider-item class incase we want to style it.
+    image.classList.add('slider-item');
 
-      const barItem = document.createElement('div');
-      barItem.setAttribute('slider-goto', i);
-      barItem.classList.add('slider-bar');
-      bars.append(barItem);
-    }
+    // Append the clone to the slider div
+    document.getElementById('slider').append(image);
+
+    // Create a barItem div
+    const barItem = document.createElement('div');
+    // Set the slider metadata
+    barItem.setAttribute('slider-goto', i);
+    // Add the slider-bar class
+    barItem.classList.add('slider-bar');
+    // Append it to the slider-bars
+    bars.append(barItem);
+  }
 
 
-    const slider = new Siema({
-      selector: '#slider',
-      duration: 200,
-      onInit() {
-        const barItems = document.querySelectorAll('#slider-bars > div');
-        console.log(barItems);
-        for (let i = 0; i < barItems.length; i++) {
-          if (this.currentSlide === i) {
-            barItems[i].classList.add('slider-bar-active');
-          }
-          barItems[i].classList.remove('slider-bar-active');
-          barItems[i].addEventListener('click', function () {
-            slider.goTo(i);
-          });
-        }
-      },
-      onChange() {
-        const barItems = document.querySelectorAll('#slider-bars > div');
-        for(let i = 0; i < barItems.length; i++) {
-          barItems[i].classList.remove('slider-bar-active');
-          if(this.currentSlide === i) {
-            barItems[i].classList.add('slider-bar-active');
-          }
+  // Instantiate the slider
+  const slider = new Siema({
+    selector: '#slider',
+    duration: 200,
+    onInit() {
+      const barItems = document.querySelectorAll('#slider-bars > div');
+      console.log(barItems);
+      for (let i = 0; i < barItems.length; i++) {
+        barItems[i].classList.remove('slider-bar-active');
+        barItems[i].addEventListener('click', function () {
+          slider.goTo(i);
+        });
+        if (this.currentSlide === i) {
+          barItems[i].classList.add('slider-bar-active');
         }
       }
-    });
-    console.log(slider);
-  }
+    },
+    onChange() {
+      const barItems = document.querySelectorAll('#slider-bars > div');
+      for (let i = 0; i < barItems.length; i++) {
+        barItems[i].classList.remove('slider-bar-active');
+        if (this.currentSlide === i) {
+          barItems[i].classList.add('slider-bar-active');
+        }
+      }
+    }
+  });
+
+  /* Masonry Actions */
+
+  const grid = document.getElementById('grid');
+
+  // Using a lambda (arrow function) won't work here because it ruins the this scope.
+  imagesLoaded(grid, function () {
+    new Masonry(grid);
+  });
 }
