6acec1b7fce940e46ebdea605cd86e9615909e6b
Mythie <mythiedev@gmail.com>
2018-10-29 00:21:43 +1100

    next steps

--- js/main.js
+++ js/main.js
@@ -14,9 +14,33 @@ window.onload = function () {
     // Get the slider
     const images = document.querySelectorAll('.image-item');
     const bars = document.getElementById('slider-bars');
+
+    for (let i = 0; i < images.length; i++) {
+      document.getElementById('slider').append(images[i]);
+
+      const barItem = document.createElement('div');
+      barItem.setAttribute('slider-goto', i);
+      barItem.classList.add('slider-bar');
+      bars.append(barItem);
+    }
+
+
     const slider = new Siema({
       selector: '#slider',
-      duration: 1000,
+      duration: 200,
+      onInit() {
+        const barItems = document.querySelectorAll('#slider-bars > div');
+        console.log(barItems);
+        for (let i = 0; i < barItems.length; i++) {
+          if (this.currentSlide === i) {
+            barItems[i].classList.add('slider-bar-active');
+          }
+          barItems[i].classList.remove('slider-bar-active');
+          barItems[i].addEventListener('click', function () {
+            slider.goTo(i);
+          });
+        }
+      },
       onChange() {
         const barItems = document.querySelectorAll('#slider-bars > div');
         for(let i = 0; i < barItems.length; i++) {
@@ -27,21 +51,6 @@ window.onload = function () {
         }
       }
     });
-
-    for(let i = 0; i < images.length; i++) {
-      slider.append(images[i]);
-
-      const barItem = document.createElement('div');
-      barItem.setAttribute('slider-goto', i);
-      barItem.classList.add('slider-bar');
-      if(i === 0) {
-        barItem.classList.add('slider-bar-active');
-      }
-      barItem.addEventListener('click', function () {
-        slider.goTo(i);
-      });
-
-      bars.append(barItem);
-    }
+    console.log(slider);
   }
 }
